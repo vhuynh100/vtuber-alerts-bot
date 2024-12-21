1696ba700fafdb08aa5fc8761ffe5730a9f38314
vhuynh100 <vanehuyn@gmail.com>
2024-12-21 00:35:34 -0600

    feat: bot alerts can send to multiple channels

--- main.py
+++ main.py
@@ -14,15 +14,21 @@ load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
 
-# List of YouTube channel IDs (multiple VTubers)
+company_icons = {
+    "hololive": "https://upload.wikimedia.org/wikipedia/commons/4/48/Hololive_logo.png",
+    "nijisanji": "https://upload.wikimedia.org/wikipedia/commons/archive/7/73/20240308191045%21Nijisanji_Logo.png",
+    "indie": "",
+}
+
+# List of YouTube channel IDs
 youtube_channel_ids = [
-    ## Nijisanji EN
-    "UCIeSUTOTkF9Hs7q3SGcO-Ow", # Elira Pendora
+    # Nijisanji EN
+    # "UCIeSUTOTkF9Hs7q3SGcO-Ow", # Elira Pendora
     # "UCwaS8_S7kMiKA3izlTWHbQg",  # Maria Marionette
     # "UCBURM8S4LH7cRZ0Clea9RDA", # Reimu Endou
     # "UChKXd7oqD18qiIYBoRIHTlw", # Meloco Kyoran
 
-    ## Hololive EN Girls
+    # # Hololive EN Girls
     # Myth
     # "UCHsx4Hqa-1ORjQTh9TYDhww",  # Takanashi Kiara
     # "UCL_qhgtOy0dy1Agp8vkySQg",  # Mori Calliope
@@ -56,10 +62,22 @@ intents.message_content = True
 bot = discord.Client(intents=intents)
 tree = discord.app_commands.CommandTree(bot) # Initialize CommandTree for slash commands
 
-routes = {} # {discord_channel_id: [yt_channel_1, yt_channel_2, ...]}
+class Streamer:
+    def __init__(self, channel_id, name, company):
+        self.channel_id = channel_id
+        self.name = name
+        self.company = company
+
+assignments = {
+    #       discord_channel_id_1:
+    #       {
+    #           "streamers": [Streamer_1, Streamer_2, ...],
+    #           "checked_videos": set()
+    #       },
+}
 
 # Track video IDs already checked
-checked_videos = set()
+# checked_videos = set()
 
 def fetch_recent_video_ids(channel_id):
     """Fetch recent video IDs from a YouTube channel's RSS feed."""
@@ -107,46 +125,47 @@ def check_videos_live(video_ids):
 
 async def check_for_live_streams():
     """Fetch recent videos, check if they are live, and notify Discord."""
-    global checked_videos, routes
+    global assignments
 
-    for channel_id in youtube_channel_ids:
-        video_ids = fetch_recent_video_ids(channel_id)
+    for discord_channel_id, data in assignments.items():
+        streamers = data.get("streamers", [])
+        channel_checked_videos = data.get("checked_videos", set())
         
-        new_videos = [video for video in video_ids if video not in checked_videos] # Filter out already-checked videos
-        if not new_videos:
-            # print("skipping channel because no new videos to check...") # TODO: Delete
-            continue  # Skip if no new videos to check
-        
-        # print("new videos detected. checking if videos are live...") # TODO: Delete
-        live_videos = check_videos_live(new_videos)
-
-        for video_id, channel_title, title, link in live_videos:
-            checked_videos.update(new_videos)
+        for streamer in streamers:
+            video_ids = fetch_recent_video_ids(streamer.channel_id)
             
-            # Notify only the appropriate Discord channels
-            for discord_channel_id, streamer_channel_ids in routes.items():
-                if channel_id in streamer_channel_ids:
-                    channel = bot.get_channel(discord_channel_id)
-                    if channel:
-                        embed = discord.Embed(
-                            color=discord.Color.blue(),
-                            title="LIVE on YouTube",
-                            url=link,
-                            description=title,
-                        )
-                        embed.set_author(
-                            name=channel_title,
-                        )
-                        embed.set_thumbnail(
-                            # url="https://upload.wikimedia.org/wikipedia/commons/4/48/Hololive_logo.png" # TODO: Dynamically add logo
-                            url="https://upload.wikimedia.org/wikipedia/commons/archive/7/73/20240308191045%21Nijisanji_Logo.png"
-                        )
-                        embed.add_field(name=":clock3: Live", value=f"<t:{int(time.time())}:R>", inline=True)
-                        # embed.add_field(name="Viewers", value=f"{} watching now", inline=True) # TODO: use "concurrentViewers" field in YT API response JSON
-                        thumbnail_url = f"https://img.youtube.com/vi/{video_id}/hqdefault.jpg"
-                        embed.set_image(url=thumbnail_url)
-                        # embed.set_footer(text="Youtube • 7/30/2023 4:01 PM") # TODO: Add stream date
-                        await channel.send(embed=embed)
+            new_videos = [video for video in video_ids if video not in channel_checked_videos] # Filter out already-checked videos
+            if not new_videos:
+                print(f"skipping {streamer.name} because no new videos to check...") # TODO: Delete
+                continue  # Skip if no new videos to check
+            
+            print(f"new videos detected for {streamer.name}. checking if videos are live...") # TODO: Delete
+            live_videos = check_videos_live(new_videos)
+            channel_checked_videos.update(new_videos)
+
+            for video_id, channel_title, title, link in live_videos:
+                print(f"notifying about {streamer.name}'s video {title}") # TODO: Delete
+                # Notify only the appropriate Discord channels
+                channel = bot.get_channel(discord_channel_id)
+                if channel:
+                    embed = discord.Embed(
+                        color=discord.Color.blue(),
+                        title="LIVE on YouTube",
+                        url=link,
+                        description=title,
+                    )
+                    embed.set_author(
+                        name=channel_title,
+                    )
+                    embed.set_thumbnail(
+                        url=company_icons.get(streamer.company, "")
+                    )
+                    embed.add_field(name=":clock3: Live", value=f"<t:{int(time.time())}:R>", inline=True)
+                    # embed.add_field(name="Viewers", value=f"{} watching now", inline=True) # TODO: use "concurrentViewers" field in YT API response JSON
+                    thumbnail_url = f"https://img.youtube.com/vi/{video_id}/hqdefault.jpg"
+                    embed.set_image(url=thumbnail_url)
+                    # embed.set_footer(text="Youtube • 7/30/2023 4:01 PM") # TODO: Add stream date
+                    await channel.send(embed=embed)
 
 def get_channel_name(channel_id: str) -> str:
     """Fetch the channel name from YouTube's RSS feed."""
@@ -166,69 +185,109 @@ def get_channel_name(channel_id: str) -> str:
     
     return "Unknown"
 
-def save_routes():
-    """Save routes to a file to persist routes dict"""
-    with open("routes.json", "w") as f:
-        json.dump(routes, f)
+def save_assignments():
+    """Save assignments to a file to persist assignments dict"""
+    with open("assignments.json", "w") as f:
+        json.dump(
+            {
+                k: {
+                    "streamers": [{"channel_id": s.channel_id, "name": s.name, "company": s.company} for s in v["streamers"]],
+                    "checked_videos": list(v["checked_videos"]),
+                }
+                for k, v in assignments.items()
+            },
+            f,
+        )
 
-def load_routes():
-    """Load routes from file"""
-    global routes
+def load_assignments():
+    """Load assignments from file"""
+    global assignments
     try:
-        with open("routes.json", "r") as f:
-            routes = json.load(f)
+        with open("assignments.json", "r") as f:
+            data = json.load(f)
+            assignments = {
+                int(k): {
+                    "streamers": [Streamer(s["channel_id"], s["name"], s.get("company", "indie")) for s in v.get("streamers", [])],
+                    # "checked_videos": set(v.get("checked_videos", [])), TODO: Decide if we want this or empty
+                    "checked_videos": set(), # TODO: Empty for testing
+                }
+                for k, v in data.items()
+            }
     except FileNotFoundError:
-        routes = {}
+        assignments = {}
 
 """Discord Bot Functions"""
-# @tree.command(name="route", description="Configure a route")
-# async def configure_route(interaction: discord.Interaction):
-    # Configure a route (view list)
+@tree.command(name="listassignments", description="List the streamers assigned to the Discord channel")
+async def list_assignments(interaction: discord.Interaction):
+    # View list of assignments
+    discord_channel_id = interaction.channel_id
+    channel_assignments = assignments.get(discord_channel_id, [])
+    
+    assignment_names = [f"`{streamer.name}`" for streamer in channel_assignments.get("streamers")]
+    formatted_names = ", ".join(assignment_names) if assignment_names else "No streamers assigned."
+    await interaction.response.send_message(f"✅ This Discord channel is subscribed to notifications for: {formatted_names}")
 
-@tree.command(name="assign", description="Assign a streamer to a route")
-async def assign_to_route(interaction: discord.Interaction, streamer_channel_id: str):
+
+@tree.command(name="assign", description="Assign a streamer to a Discord channel")
+async def assign_to_discord_channel(interaction: discord.Interaction, streamer_channel_id: str):
     """ Add a YouTube channel ID to this Discord channel's notification list. """
     # 2 options: channel (string: channel), mention (string: role or "None")
-    global routes
+    global assignments
     discord_channel_id = interaction.channel_id
 
     streamer_name = get_channel_name(streamer_channel_id)
-
-    if discord_channel_id not in routes:
-        routes[discord_channel_id] = []
-
-    if streamer_channel_id not in routes[discord_channel_id]:
-        routes[discord_channel_id].append(streamer_channel_id)
-        await interaction.response.send_message(f"✅ Subscribed to notifications for `{streamer_name}` in this Discord channel.")
+    if streamer_name == "Unknown":
+        await interaction.response.send_message(f"⚠️ Could not fetch channel name. Please try again later.")
+        return
+    
+    if "hololive" in streamer_name.lower():
+        streamer_company = "hololive"
+    elif "nijisanji" or "にじさんじ" in streamer_name.lower():
+        streamer_company = "nijisanji"
     else:
-        await interaction.response.send_message(f"⚠️ This channel is already subscribed to notifications for `{streamer_name}`.")
+        streamer_company = "indie"
 
-    save_routes()
+    if discord_channel_id not in assignments:
+        assignments[discord_channel_id] = {"streamers": [], "checked_videos": set()}
 
-@tree.command(name="unassign", description="Remove a streamer from a route")
-async def unassign_from_route(interaction: discord.Interaction, streamer_channel_id: str):
+    streamer_to_assign = Streamer(streamer_channel_id, streamer_name, streamer_company)
+
+    if streamer_to_assign not in assignments[discord_channel_id]["streamers"]:
+        assignments[discord_channel_id]["streamers"].append(streamer_to_assign)
+        await interaction.response.send_message(f"✅ Subscribed to notifications for `{streamer_to_assign.name}` in this Discord channel.")
+    else:
+        await interaction.response.send_message(f"⚠️ This channel is already subscribed to notifications for `{streamer_to_assign.name}`.")
+
+    save_assignments()
+
+@tree.command(name="unassign", description="Remove a streamer from a Discord channel")
+async def unassign_from_discord_channel(interaction: discord.Interaction, streamer_channel_id: str):
     """ Remove a YouTube channel ID from this Discord channel's notification list. """
-    global routes
+    global assignments
     discord_channel_id = interaction.channel_id
 
     streamer_name = get_channel_name(streamer_channel_id)
 
-    if discord_channel_id in routes and streamer_channel_id in routes[discord_channel_id]:
-        routes[discord_channel_id].remove(streamer_channel_id)
-        await interaction.response.send_message(f"✅ Unsubscribed from notifications for `{streamer_name}` in this Discord channel.")
+    for streamer in assignments[discord_channel_id]:
+        if streamer.channel_id == streamer_channel_id:
+            assignments[discord_channel_id].remove(streamer)
+            await interaction.response.send_message(f"✅ Unsubscribed from notifications for `{streamer_name}` in this Discord channel.")
+            if assignments[discord_channel_id] == []:
+                del assignments[discord_channel_id]
+            break
     else:
         await interaction.response.send_message(f"⚠️ This channel is not subscribed to `{streamer_name}`.")
 
-    save_routes()
+    save_assignments()
 
-@tasks.loop(seconds=15) # TODO: Change to 3 minutes when done testing
+@tasks.loop(seconds=5) # TODO: Change to 3 minutes when done testing
 async def periodic_live_stream_check():
     await check_for_live_streams()
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
-    load_routes()
+    load_assignments()
     await tree.sync() # Sync commands to the server
     periodic_live_stream_check.start()
 
